142113b6d9852299801676f62b4905af2d266606
Dlilbeast <dmc.lilbeast@gmail.com>
2024-11-04 15:39:21 -0600

    Implemented platform placement
    Placement is currently not limited

--- PlatformerAcradeProject/Assets/BuilderController.cs
+++ PlatformerAcradeProject/Assets/BuilderController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,12 +10,16 @@ public class BuilderController : MonoBehaviour
     public InputAction moveX;
     public InputAction build;
     public InputAction jump;
+    public GameObject platform;
     Rigidbody2D rigidbody2d;
+    public Grid grid;
     public float acceleration;
     public float speed;
     public float drag;
     public float jumpForce;
     public float depth;
+    private float buildCooldown = 0;
+    private int facing = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +34,7 @@ public class BuilderController : MonoBehaviour
     {
         float move = moveX.ReadValue<float>();
         bool jumpInput = jump.ReadValue<float>() == 1;
+        bool buildInput = build.ReadValue<float>() > 0;
         RaycastHit2D groundCheck = Physics2D.Raycast(transform.position, Vector2.down, depth, 8);
         rigidbody2d.AddForce(Vector2.right*move*acceleration);
         if (groundCheck.collider != null && jumpInput)
@@ -45,6 +51,13 @@ public class BuilderController : MonoBehaviour
             {
                 rigidbody2d.AddForce(Vector2.right * drag * acceleration);
             }
+        }else if(move > 0)
+        {
+            facing = 1;
+        }
+        else
+        {
+            facing = -1;
         }
         if(rigidbody2d.velocity.x > speed)
         {
@@ -53,6 +66,43 @@ public class BuilderController : MonoBehaviour
         if (rigidbody2d.velocity.x < speed * -1)
         {
             rigidbody2d.velocity = new Vector2(speed * -1, rigidbody2d.velocity.y);
+        }
+        if (buildCooldown == 0)
+        {
+            if (buildInput)
+            {
+                if (groundCheck.collider == null)
+                {
+                    RaycastHit2D checkPos = 
+                        Physics2D.Raycast(grid.CellToWorld(grid.WorldToCell(transform.position + new Vector3(0, -1.0f, 0))) +
+                        new Vector3(grid.cellSize.x / 2, grid.cellSize.y / 2, 0), Vector2.down, 0.01f, 8);
+                    if (checkPos.collider == null)
+                    {
+                        Instantiate(platform,
+                            grid.CellToWorld(grid.WorldToCell(transform.position + new Vector3(0, -1.0f, 0))) +
+                            new Vector3(grid.cellSize.x / 2, grid.cellSize.y / 2, 0),
+                            quaternion.identity);
+                    }
+                }
+                else
+                {
+                    RaycastHit2D checkPos = Physics2D.Raycast(grid.CellToWorld(grid.WorldToCell(transform.position +
+                        new Vector3(grid.cellSize.x * facing, -1.0f, 0))) + 
+                        new Vector3(grid.cellSize.x / 2, grid.cellSize.y / 2, 0), Vector2.down, 0.01f, 8);
+                    if (checkPos.collider == null)
+                    {
+                        Instantiate(platform,
+                            grid.CellToWorld(grid.WorldToCell(transform.position +
+                            new Vector3(grid.cellSize.x * facing, -1.0f, 0))) +
+                            new Vector3(grid.cellSize.x / 2, grid.cellSize.y / 2, 0),
+                            quaternion.identity);
+                    }
+                }
+                buildCooldown = 100;
+            }
+        }else if(buildCooldown > 0)
+        {
+            buildCooldown--;
         }
     }
 }
--- PlatformerAcradeProject/Assets/Platform.cs
+++ PlatformerAcradeProject/Assets/Platform.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void slimed()
+    {
+        spriteRenderer.sprite = sprites[1];
     }
 }
